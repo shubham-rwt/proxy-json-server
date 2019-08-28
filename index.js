45502a5ae0fef5ef12808c43d3504e4c1cc11fa2
shubham-rwt <shubham.rawat@greatwits.com>
2019-08-28 18:02:46 +0530

    cors

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ const config = require('./config.json');
 const port = config.mock_server;
 const json_port = config.json_server;
 
-const proxy = http.createServer((reqProxy, resProxy)=>{
-    console.log("url", reqProxy.url);
-    resProxy.setHeader("Content-Type","application/json");
-    
-    http.get(`http://localhost:${config.json_server}${reqProxy.url}`,function(res){
+const proxy = http.createServer((reqProxy, resProxy) => {
+  console.log("url", reqProxy.url);
+  resProxy.setHeader('Access-Control-Allow-Origin', '*');
+  resProxy.setHeader('Access-Control-Request-Method', '*');
+  resProxy.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  resProxy.setHeader('Access-Control-Allow-Headers', '*');
+  resProxy.setHeader("Content-Type", "application/json");
+
+  http.get(`http://localhost:${config.json_server}${reqProxy.url}`, function (res) {
     const { statusCode } = res;
     const contentType = res.headers['content-type'];
-  
+
     let error;
     if (statusCode !== 200) {
       error = new Error('Request Failed.\n' +
-                        `Status Code: ${statusCode}`);
+        `Status Code: ${statusCode}`);
     } else if (!/^application\/json/.test(contentType)) {
       error = new Error('Invalid content-type.\n' +
-                        `Expected application/json but received ${contentType}`);
+        `Expected application/json but received ${contentType}`);
     }
     if (error) {
       console.error(error.message);
@@ -29,7 +33,7 @@ const proxy = http.createServer((reqProxy, resProxy)=>{
       res.resume();
       return;
     }
-  
+
     res.setEncoding('utf8');
     let rawData = '';
     res.on('data', (chunk) => { rawData += chunk; });
@@ -40,19 +44,19 @@ const proxy = http.createServer((reqProxy, resProxy)=>{
       } catch (e) {
         resProxy.write('{"error":"true"}');
         resProxy.end();
-    }
+      }
     });
-   });
+  });
 });
 
-proxy.listen((port),function(){
-    console.log(`json-server db.json --port ${json_port}`);
-    exec(`json-server db.json --port ${json_port}`, function callback(error, stdout, stderr){
-        if(error){
-            console.log(error);
-            process.exit();
-        }
-        console.log("running on port " + json_port);
-    });
-    console.log("running mock server in port " + port);
+proxy.listen((port), function () {
+  console.log(`json-server db.json --port ${json_port}`);
+  exec(`json-server db.json --port ${json_port}`, function callback(error, stdout, stderr) {
+    if (error) {
+      console.log(error);
+      process.exit();
+    }
+    console.log("running on port " + json_port);
+  });
+  console.log("running mock server in port " + port);
 });
